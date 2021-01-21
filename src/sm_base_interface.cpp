23154398c45536dc42276c24252169019eb944df
Trevor Stanca <tstanca@students.kennesaw.edu>
2021-01-20 21:08:05 -0500

    for noah <3

--- src/sm_base_interface.cpp
+++ src/sm_base_interface.cpp
@@ -9,10 +9,21 @@ void smBaseInterface::onInit() {
     running_ = true;
     deviceThread_ = boost::shared_ptr< boost::thread > 
         (new boost::thread(boost::bind(&smBaseInterface::control, this)));
+
+    // subscribe topic: sm_switch type: std_msgs/String
+
+    // publishers topic: <state_name>/activate type: std_msgs/Bool
 }
 void smBaseInterface::control() {
     while(running_) {
     std::cout<<"testing\n";
+        // check if current state is different from wanted state
+        // check if wanted transition is legal 
+            // if not legal: switch wanted state -> current
+                // return 
+            // else: proceed
+        // turn off old state by sending false
+        // turn on new state by sending true 
     }
 }
 } // namespace statemachine
