8534fc00b9ff03c73a5d5fef5e0bfc1a6f611f69
Trevor Stanca <tstanca@students.kennesaw.edu>
2021-01-23 15:11:48 -0500

    wrote base state machine base file

---
+++ include/statemachine/base_state_machine.h
@@ -0,0 +1,11 @@
+#ifndef BASE_STATE_MACHINE_H 
+#define BASE_STATE_MACHINE_H
+namespace state_machine {
+
+class state {
+public:
+private:
+protected:
+}; //class state
+} // namespace statemachine
+#endif // BASE_STATE_MACHINE_H
