e27c3fb6f1332572c665a1abb35b766ab2be8d65
Trevor Stanca <tstanca@students.kennesaw.edu>
2021-02-01 19:35:36 -0500

    added base_sm files

--- include/statemachine/base_state_machine.h
+++ include/statemachine/base_state_machine.h
@@ -2,12 +2,22 @@
 #define BASE_STATE_MACHINE_H
 
 // includes 
+#include <utility> // std::pair
+#include <vector>
+#include <string>
 
+using namespace std;
 namespace state_machine {
 
-class state {
+class stateMachine {
 public:
+    void transition();
+    pair<string, char> get_state();
+    vector<string> get_all_states();
+    
 private:
+    vector<pair<string, char>> states;
+    int curr_state;  // index in vector above
 protected:
 }; //class state
 } // namespace statemachine
